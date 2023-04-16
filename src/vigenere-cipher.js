db8a5142f3a81dd6619fb74ab66fba35f83ad48b
McQueen <m.gorelenko@mail.ru>
2023-04-16 14:23:33 +0300

    feat: some solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+encrypt(message, key) {
+	if (!message || !key) {
+		throw new Error('Incorrect arguments!');
+	}
+}
+decrypt(message, key) {
+	if (!message || !key) {
+		throw new Error('Incorrect arguments!');
+	}
+}
+
 }
 
 module.exports = {
